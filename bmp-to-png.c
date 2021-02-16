f432dc541ba4c2cf7d1be03635167aed537bf667
Hasanabbas-Momin <Hasanabbas.Momin@iiitb.org>
2021-02-16 20:27:27 +0530

    start of 3rd task

---
+++ bmp-to-png.c
@@ -0,0 +1,7 @@
+#include "bmp_ds_new.h"
+#include "png_ds.h"
+int main()
+{
+    IHDR.width = BMP.b_img_info.width                // change BMP to the name of struct
+    IHDR.height = BMP.b_img_info.height
+}
