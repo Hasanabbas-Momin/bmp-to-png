c6c3d9700a7bf74e6d52e64cc80b74994d8c3fe4
Vishnutha <Vishnutha.Sheela@iiitb.org>
2021-02-19 14:12:01 +0530

    Update bmp-to-png.c

--- bmp-to-png.c
+++ bmp-to-png.c
@@ -1,7 +1,26 @@
 #include "bmp_ds_new.h"
 #include "png_ds.h"
-int main()
+#include "Read_BMP.h"
+void BMP_TO_PNG()
 {
-    IHDR.width = BMP.b_img_info.width                // change BMP to the name of struct
-    IHDR.height = BMP.b_img_info.height
+    struct HEADER h1;
+    h1.first_byte=89;
+    h1.PNG_SIGN[3]="PNG";
+    h1.random1=3338;
+    h1.random2=26;
+    h1.random3=10;
+    struct IHDR I1;
+    I1.size=Read_BMPfile.bmp.hdr.file_size;
+    I1.str="IHDR";
+    I1.width=Read_BMPfile.img_info.width;
+    I1.height=Read_BMPfile.img_info.height;
+    I1.bit_depth=8;
+    I1.colour_type=0;
+    I1.cm=0;
+    I1.fm=0;
+    I1.im=0;
+    struct IDAT I2;
+    I2.size==Read_BMPfile.img_info.header_size;
+    I2.str="IDAT";
+
 }
