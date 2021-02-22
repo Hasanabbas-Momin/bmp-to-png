4581ea3efd58d9592bc1de3c6fa2baff68cfde51
markzuccu175 <pranav.vajreshwari@iiitb.org>
2021-02-22 17:51:23 +0530

    Update target_png.c

--- target_png.c
+++ target_png.c
@@ -9,11 +9,14 @@ void writepng(struct HEADER,struct IHDR, struct IDAT, int*);
 void writepng(struct HEADER h1,struct IHDR I1,struct IDAT I2,IEND)
 {
    File *ft = fopen("convertedimage.png","wb");
+   
    if (ft == NULL)
     return 1;
-   fwrite(h1,sizeof(HEADER),1,ft);
+   
+    fwrite(h1,sizeof(HEADER),1,ft);
    fwrite(I1,sizeof(struct IHDR),1,ft);
    fwrite(I2,sizeof(struct(IDAT),1,ft);
    fwrite(IEND,4*sizeof(char),1,ft);   
    
+   fclose(ft);
 }
