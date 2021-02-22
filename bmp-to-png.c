f61e153103be006ac824c69a3d807dbbd4bb241e
Hasanabbas-Momin <75903359+Hasanabbas-Momin@users.noreply.github.com>
2021-02-22 14:51:59 +0530

    made necessary changes integrability of programs

--- bmp-to-png.c
+++ bmp-to-png.c
@@ -1,11 +1,13 @@
-#include "bmp_ds_new.h"
+// #include "bmp_ds_new.h"
 #include "png_ds.h"
 #include "Read_BMP.h"
+#include<string.h>
 void BMP_TO_PNG()
 {
     struct HEADER h1;
     h1.first_byte = 137;
-    h1.PNG_SIGN[3] = "PNG";
+    // h1.PNG_SIGN[3] = "PNG";
+    strcpy(h1.PNG_SIGN, "PNG");
     h1.random1 = 3338;
     h1.random2 = 26;
     h1.random3 = 10;
@@ -14,9 +16,10 @@ void BMP_TO_PNG()
     struct IHDR I1;
     // I1.size = Read_BMPfile.bmp.hdr.file_size;     // this is equal to 13 so no need to take it
     I1.size = 13;
-    I1.str = "IHDR";
-    I1.width = Read_BMPfile.img_info.width;
-    I1.height = Read_BMPfile.img_info.height;
+    // I1.str = "IHDR";
+    strcpy(I1.str, "IHDR");
+    I1.width = img_info.width;
+    I1.height = img_info.height;
     I1.bit_depth = 8;
     I1.colour_type = 0;
     I1.cm = 0;
@@ -24,12 +27,12 @@ void BMP_TO_PNG()
     I1.im = 0;
 
     struct IDAT I2;
-    I2.size = Read_BMPfile.img_info.header_size;
-    I2.str = "IDAT";
+    I2.size = img_info.header_size;
+    // I2.str = "IDAT";
+    strcpy(I2.str, "IDAT");
 
-
-    
-    IEND[4] = "IEND";
+    // IEND[] = {"I", "E", "N", "D"};
+    strcpy(IEND, "IEND");
 }
 
     
